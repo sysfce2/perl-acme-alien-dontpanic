e4c0db6262f8695a9c0aef2a4aad8f44ac307091
Graham Ollis <plicease@cpan.org>
2020-01-29 10:43:43 -0700

    migrate to dzil

--- lib/Acme/Alien/DontPanic.pm
+++ lib/Acme/Alien/DontPanic.pm
@@ -2,18 +2,14 @@ package Acme::Alien::DontPanic;
 
 use strict;
 use warnings;
+use base qw( Alien::Base );
 
-our $VERSION = '1.96';
-
-use parent 'Alien::Base';
+# ABSTRACT: Test Module for Alien::Base::ModuleBuild
+# VERSION
 
 1;
 
 __END__
-
-=head1 NAME
-
-Acme::Alien::DontPanic - Test Module for Alien::Base
 
 =head1 DESCRIPTION
 
@@ -38,23 +34,6 @@ L<Alien>
 L<Acme::Ford::Prefect>
 
 =back
-
-=head1 SOURCE REPOSITORY
-
-L<http://github.com/Perl5-Alien/Acme-Alien-DontPanic>
-
-=head1 AUTHOR
-
-Original author: Joel Berger, E<lt>joel.a.berger@gmail.comE<gt>
-
-Current maintainer: Graham Ollis E<lt>plicease@cpan.orgE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2012 by Joel Berger
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
 
 =cut
 
